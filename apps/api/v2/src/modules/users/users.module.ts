86661efb356ba8a06d1c3ebd4880f79f7b770556
spatnaik-plab <spatnaik@precedentlab.com>
2026-05-02 13:46:21 -0700

    feat: register UsersController in UsersModule

--- apps/api/v2/src/modules/users/users.module.ts
+++ apps/api/v2/src/modules/users/users.module.ts
@@ -1,12 +1,14 @@
 import { EventTypesModule_2024_06_14 } from "@/platform/event-types/event-types_2024_06_14/event-types.module";
 import { PrismaModule } from "@/modules/prisma/prisma.module";
 import { TokensModule } from "@/modules/tokens/tokens.module";
+import { UsersController } from "@/modules/users/users.controller";
 import { UsersService } from "@/modules/users/services/users.service";
 import { UsersRepository } from "@/modules/users/users.repository";
 import { Module } from "@nestjs/common";
 
 @Module({
   imports: [PrismaModule, EventTypesModule_2024_06_14, TokensModule],
+  controllers: [UsersController],
   providers: [UsersRepository, UsersService],
   exports: [UsersRepository, UsersService],
 })
